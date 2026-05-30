238f8f9d648a924b95946d51f24d7782defb95ba
Surabodee <surabodee.sitc@gmail.com>
2026-05-30 10:30:22 +0700

    delete : trash

--- Part10/server/app.js
+++
@@ -1,8 +0,0 @@
-const { printModulePath } = require('./printModulePath');
-const { readHTML } = require('./readHTMLFile');
-
-console.log("ทดสอบ ส่วนที่ 1 ข้อ 3");
-printModulePath();
-
-console.log("\nทดสอบ ส่วนที่ 1 ข้อ 4");
-readHTML('lec10.html');
